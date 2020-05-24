de4b2ae4c2ce4adda7fe3ad0bfc8a991902222c4
bruce <bruce_qytl@163.com>
2020-05-24 18:31:58 +0800

    Merge branch 'dev'
    # Conflicts:
    #	git_test_project/src/main/java/com/itbruce/domain/User.java

---
+++ git_test_project/src/main/java/com/itbruce/common/TestColor.java
@@ -0,0 +1,8 @@
+package com.itbruce.common;
+
+public class TestColor {
+    public static void main(String[] args) {
+        Color yello = Color.YELLO;
+        System.out.println(yello);
+    }
+}
